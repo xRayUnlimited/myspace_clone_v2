082615d901de2a3c53864765f563951a81cc7733
Ray Suyoto <ray.suy@icloud.com>
2018-04-25 23:58:14 -0600

    posts

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,12 +1,12 @@
 const posts = (state = [], action) => {
   switch(action.type) {
     case 'GET_POSTS':
-    return action.posts;
+      return action.posts;
     case 'ADD_POST':
-    return [action.post, ...state];
+      return [action.post, ...state];
     default:
-    return state;
+      return state;
   }
- }
- 
- export default posts;
+}
+
+export default posts;
